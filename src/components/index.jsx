12b7eb40781a1130f2c50cb280fa95461c72901d
Yosi Izhak <yosii84@gmail.com>
2021-04-01 12:39:23 +0300

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import React, {  useState,useRef } from 'react';
+
+
+const Dogs = () => {
+    const [colorOne, setColorOne] = useState(false);
+    const [colorTwo, setColorTwo] = useState(false);
+  
+  
+   
+  
+    return (
+        <div>
+     </div>
+    );
+  };
+  
+  export default Dogs;
